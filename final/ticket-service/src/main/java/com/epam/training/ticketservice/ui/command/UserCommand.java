144b9a705d0659a265e0795f9b8c3d0a0fb3fe26
Balogh Kristóf <baloghkristof1000@gmail.com>
2023-11-04 14:59:47 +0100

    Implemented the methods inside UserCommand

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/command/UserCommand.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/command/UserCommand.java
@@ -1,8 +1,8 @@
 package com.epam.training.ticketservice.ui.command;
 
 
+import com.epam.training.ticketservice.core.User.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
@@ -10,24 +10,26 @@ import org.springframework.shell.standard.ShellMethod;
 @AllArgsConstructor
 public class UserCommand {
 
+    private final UserService userService;
 
     @ShellMethod(key = "sign in privileged", value = "Sign in")
-    public String signIn(String username, String password){return username;
+    public String signIn(String username, String password){
+        return userService.signin(username,password)
+                .map(User -> User + " is signed in!")
+                .orElse("Wrong username or password!");
     }
 
     @ShellMethod(key = "sign out", value = "Sign out")
     public String signOut(){
-        return null;
+
+        return userService.signout()
+                .map(User -> User + " is signed out!")
+                .orElse("You need to login first!");
     }
 
     @ShellMethod(key = "describe account", value = "Get account information")
     public String describe(){
+        //return userService.describe();
         return null;
     }
-
-    @ShellMethod(key = "exit", value = "Exit.")
-    public String exit(){
-        return null;
-    }
-
 }
