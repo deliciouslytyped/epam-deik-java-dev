472b0be620dff3cf48b682f05d59648dd3ad20c2
Balogh Kristóf <baloghkristof1000@gmail.com>
2023-11-04 13:47:52 +0100

    Created user class

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/core/User.java
@@ -0,0 +1,26 @@
+package com.epam.training.ticketservice.core;
+
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    public User(String username,String password){
+        this.username = username;
+        this.password = password;
+    }
+}
