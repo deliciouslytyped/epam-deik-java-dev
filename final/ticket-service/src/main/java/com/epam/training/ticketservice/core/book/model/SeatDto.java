8260ca44588e58bcfd63b7ba7f12319bff977ede
Balogh Kristóf <baloghkristof1000@gmail.com>
2023-11-26 17:11:47 +0100

    Deleted booking

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/core/book/model/SeatDto.java
+++
@@ -1,9 +0,0 @@
-package com.epam.training.ticketservice.core.book.model;
-
-import com.epam.training.ticketservice.core.book.persistence.Seat;
-
-public record SeatDto(int row, int col) {
-    public SeatDto(Seat seat) {
-        this(seat.getRows(), seat.getCols());
-    }
-}
